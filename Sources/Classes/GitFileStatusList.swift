c41aac70d4500829c1e35c8697968ae535b5cd27
Max Akhmatov <akhmatov.max@gmail.com>
2020-04-16 13:15:40 +0400

    Possibility to remove and replace statuses in GitFileStatusList
    Small change that allows to remove and replace statuses in GitFileStatusList via two new methods.

--- Sources/Classes/GitFileStatusList.swift
+++ Sources/Classes/GitFileStatusList.swift
@@ -23,12 +23,34 @@ public class GitFileStatusList {
         self.files = files
     }
     
+    /// Adds a new status the list if not exists
+    ///
+    /// If the given status already exists in the list, the exception is raised.
+    /// - Parameter status: A file status to add to the list
     public func add(_ status: GitFileStatus) throws {
         guard !contains(filePath: status.path) else {
             throw Exception.fileStatusAlreadyExists
         }
         
         files.append(status)
+    }
+    
+    /// Removes all statuses from the list those file paths are the same as the given status has
+    /// - Parameter status: A file status that path will be used to search all statuses to remove
+    public func remove(_ status: GitFileStatus) {
+        files.removeAll(where: {$0.path == status.path})
+    }
+    
+    /// Searches for the given status in the list and replaces it with the new one.
+    ///
+    /// The comparison is made using the  file path of the given status. Be ware. all statuses with the same file path will be replaced
+    /// - Parameter status: A file status to replace the existing one with
+    public func replace(_ status: GitFileStatus) {
+        for (index, file) in files.enumerated() {
+            if file.path == status.path {
+                files[index] = status
+            }
+        }
     }
     
     public func contains(filePath: String) -> Bool {
